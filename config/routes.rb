d8f05e5f216d4a50c7f4973d8af8a9c983d5a333
Nate Burns and Roy van de Water <dev+nate+roy@integrumtech.com>
2013-09-22 10:05:23 -0700

    add created_at to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MetSomeone::Application.routes.draw do
   resources :meetings
-
+  root :to => 'meetings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
